0fda1582993aea6db56240ac1a7300a186fa7895
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-21 16:45:52 +0900

    Solution_3 Update

---
+++ src/main/kotlin/solutions/Solution_3.kt
@@ -0,0 +1,39 @@
+package solutions
+
+// 별 찍기 -> 두개로 증가
+
+fun main() {
+
+    solution(3)
+    /*
+        *      1
+        **     2
+        ****   4
+    */
+
+    println()
+
+    solution(5)
+    /*
+        *                   1
+        **                  2
+        ****                4
+        ********            8
+        ****************    16
+    */
+
+}
+
+
+
+fun solution(star: Int) {
+    var cnt = 1
+
+    for(i in 1..star) {
+        for(j in 1..cnt) {
+            print("*")
+        }
+        println()
+        cnt *= 2
+    }
+}
