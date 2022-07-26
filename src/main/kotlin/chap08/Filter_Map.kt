f29a4b499613df58d8c9a76bbc73b10836f9963f
sungyong-lim <dlatjddyd524@gmail.com>
2022-07-26 14:47:52 +0900

    Create Filter_Map.kt

---
+++ src/main/kotlin/chap08/Filter_Map.kt
@@ -0,0 +1,32 @@
+package chap08
+
+fun main() {
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    val eager = decorations.filter { it[0] == 'p' }
+    println("filterTest : $eager")
+
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println(filtered)
+    println("filtered : ${filtered.toList()}")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access : $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val lazyMap2 = decorations.asSequence()
+        .filter { it[0] == 'p' }
+        .map {
+            println("access : $it")
+            it
+        }
+    println("-----")
+    println("filtered : ${lazyMap2.toList()}")
+
+}
